5b89e376f4f9d3fd419ef55700de1fa789a778fe
Samiya Ahmed Chowdhury <samiya.ahmed.chowdhury@gmail.com>
2026-05-09 00:52:36 +0600

    Fix gitignore and clean project

---
+++ SalesRecord.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileShopInventory
+{
+    internal class SalesRecord
+    {
+    }
+}
---
+++ SmartPhone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileShopInventory
+{
+    internal class SmartPhone:Phone
+    {
+        private int cameraMegapixels;
+        public int CameraMegapixels
+        {
+            get { return cameraMegapixels; }
+            set { if(value>0)cameraMegapixels = value; }
+        }
+
+    }
+}
